aae760900c6982f3008bbeb797cfc589c74763f0
Mike Huff <mikehuff@amazon.com>
2016-12-24 11:33:35 -0800

    Add last run of sounds

--- sounds/sounds.js
+++ sounds/sounds.js
@@ -33,8 +33,76 @@ export default function() {
       url: "tote_wrap.mp3"
     },
     {
+      name: "tote life",
+      url: "IB_tote_life.mp3"
+    },
+    {
       name: "best thing",
       url: "IB_best_thing_to_happen.mp3"
+    },
+    {
+      name: "enjoys basking",
+      url: "IB_enjoys_basking.mp3"
+    },
+    {
+      name: "believed in you",
+      url: "IB_believed_in_you.mp3"
+    },
+    {
+      name: "will coach you",
+      url: "IB_coach_you_manhood.mp3"
+    },
+    {
+      name: "still proud",
+      url: "IB_is_still_proud.mp3"
+    },
+    {
+      name: "thinks you're precious",
+      url: "IB_thinks_youre_precious.mp3"
+    },
+    {
+      name: "will not bless you",
+      url: "IB_will_not_bless_you.mp3"
+    },
+    {
+      name: "top bear now",
+      url: "IB_wants_top_bear.mp3"
+    },
+    {
+      name: "needs bug spray",
+      url: "IB_bug_spray.mp3"
+    },
+    {
+      name: "is survivor",
+      url: "IB_is_survivor.mp3"
+    },
+    {
+      name: "made her a pajmina",
+      url: "IB_made_a_pajmina.mp3"
+    },
+    {
+      name: "is survivor",
+      url: "IB_is_survivor.mp3"
+    },
+    {
+      name: "settled that",
+      url: "IB_settled_that.mp3"
+    },
+    {
+      name: "likes turtle",
+      url: "IB_turtle.mp3"
+    },
+    {
+      name: "don't ditch",
+      url: "IB_dont_ditch.mp3"
+    },
+    {
+      name: "turtlenectk",
+      url: "IB_turtle_neck.mp3"
+    },
+    {
+      name: "miss yoga",
+      url: "IB_miss_yoga.mp3"
     },
   ];
 };
